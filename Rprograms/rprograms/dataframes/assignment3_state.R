3611fd1ebcd516c4355411b326e614eaa9976527
ksathish12 <sathishk.a.12@gmail.com>
2020-03-04 15:37:17 +0530

    modified assignment3 in datafram

--- Rprograms/rprograms/dataframes/assignment3_state.R
+++ Rprograms/rprograms/dataframes/assignment3_state.R
@@ -5,7 +5,22 @@ s.division<-state.division
 s.name<-state.name
 s.region<-state.region
 s<-data.frame(s.abb,s.area,s.division,s.name,s.region)
-print(colnames(s))
-print(substring(colnames(s),3:6))
-colnames(s)<-c("abb","are","div","nam","reg")
+
+names<-colnames(s)
+#print(substring(names,1:3,1:3))
+#colnames(s)<-c("abb","are","div","nam","reg")
+#print(s)
+
+
+new1<-c() #creating the empty vector new1
+
+for (val in colnames(s))
+{
+  new<-(substring(val,3,5)) #This will create the column names with each name displaying characters from 3 to 5
+  new1[[paste0(val)]] <- new #storing the previous result in the vector new1
+  
+}
+#print(new1)
+newColname<-new1 #storing the vector new1 in the variable newColname
+colnames(s)<-c(newColname) #changing the existing column name with newColname
 print(s)
