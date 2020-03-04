3ac431a6e7b8b55a2a626f3afff412b1cc30cbee
ksathish12 <sathishk.a.12@gmail.com>
2020-03-04 14:16:17 +0530

    updated deathCode in dataframe

--- Rprograms/rprograms/dataframes/assignment2_deaths.R
+++ Rprograms/rprograms/dataframes/assignment2_deaths.R
@@ -7,8 +7,15 @@ a$Total<-total
 print(a)
 a$Mean<-mean
 print(a)
-
-
+#rm<-data.frame(a[1])
+#rf<-data.frame(a[2])
+#um<-data.frame(a[3])
+#uf<-data.frame(a[4])
+#t<-data.frame(a[5])
+#m<-data.frame(a[6])
+#final<-cbind(t,rm,rf,um,uf,m)
+final <- a[, c(5, 1, 2, 3, 4, 6)]
+print(final)
 
 #print(a)
 
