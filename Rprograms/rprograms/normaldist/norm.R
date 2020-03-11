44d78c50c7ec23a20e79a80971f0d92c45885855
ksathish12 <sathishk.a.12@gmail.com>
2020-03-11 16:14:35 +0530

    march 11 corrected norm distribution

--- Rprograms/rprograms/normaldist/norm.R
+++ Rprograms/rprograms/normaldist/norm.R
@@ -6,9 +6,9 @@ x<-seq(-4,4,length=200)
 # # plot(x,p,type="l",col="red")
 z<-(x-mean(x))/(sd(x))
 # sd(z)
-l<-sqrt((2*pi))
-m<-exp(-0.5*((x)^2))
-p2=l*m
+l<-1/sqrt((2*pi))
+m<-exp(-0.5*((z)^2))
+p2<-l*m
 print(p2)
 plot(z,p2,type="l",col="blue")
 
